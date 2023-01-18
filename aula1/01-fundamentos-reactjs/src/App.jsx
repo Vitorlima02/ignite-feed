f0c340c363b98b89e7e51c8d479727e224589629
Vitor Lima <vitorliman@gmail.com>
2023-01-18 18:29:39 -0300

    feat: impl sidebar

--- aula1/01-fundamentos-reactjs/src/App.jsx
+++ aula1/01-fundamentos-reactjs/src/App.jsx
@@ -1,9 +1,21 @@
 import { Header } from "./components/Header"
 
+import styles from './App.module.css';
+
 import './global.css'
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
-    <Header />
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          main
+        </main>
+      </div>
+    </div>
   )
 }
