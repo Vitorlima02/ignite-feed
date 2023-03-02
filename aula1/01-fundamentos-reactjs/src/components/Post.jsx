fd38532198aba63dc39e9bc6ebfc1e0226ba5b6b
Vitor Lima <vitorliman@gmail.com>
2023-03-02 11:50:00 -0300

    feat: impl css component post

--- aula1/01-fundamentos-reactjs/src/components/Post.jsx
+++ aula1/01-fundamentos-reactjs/src/components/Post.jsx
@@ -3,11 +3,13 @@ import styles from './Post.module.css';
 export function Post() {
   return (
     <article className={styles.post}>
-      <header className={styles.author}>
-        <img className={styles.avatar} src="https://github.com/Vitorlima02.png" />
-        <div className={styles.authorInfo}>
-          <strong>Vitor Lima</strong>         
-          <span>Web Developer</span>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/Vitorlima02.png" />
+          <div className={styles.authorInfo}>
+            <strong>Vitor Lima</strong>         
+            <span>Web Developer</span>
+          </div>
         </div>
 
         <time title='01 de Março às 14:30h' dateTime='2023-03-01 14:30:05'>Publicado há 1h</time>
@@ -17,7 +19,11 @@ export function Post() {
       <p>Fala galeraa 👋</p>
       <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
       <p><a href=''>👉 jane.design/doctorcare</a></p>
-      <p><a href=''>#novoprojeto #nlw #rocketseat</a></p>
+      <p>
+        <a href=''>#novoprojeto</a>{' '}
+          <a href=''>#nlw</a>{' '}
+          <a href=''>#rocketseat</a>
+      </p>
       </div>
     </article>
   )
