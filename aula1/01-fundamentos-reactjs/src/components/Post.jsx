a0ea86f3abc15f120fff28ef1b6f8909846de7db
Vitor Lima <vitorliman@gmail.com>
2023-05-03 11:29:23 -0300

    feat: add component Avatar

--- aula1/01-fundamentos-reactjs/src/components/Post.jsx
+++ aula1/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
@@ -6,7 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/Vitorlima02.png" />
+          <Avatar hasBorder src="https://github.com/Vitorlima02.png" />
           <div className={styles.authorInfo}>
             <strong>Vitor Lima</strong>         
             <span>Web Developer</span>
--- aula1/01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ aula1/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -11,7 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/Vitorlima02.png" />
+        <Avatar hasBorder src="https://github.com/Vitorlima02.png" />
         <strong>Vitor Lima</strong>
         <span>Web Delevoper</span>
       </div>
