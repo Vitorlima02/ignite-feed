aa4f39d82c70d79a732f4328bb69a9ca89180646
Vitor Lima <vitorliman@gmail.com>
2023-05-04 15:05:40 -0300

    feat: add interação ao post

--- aula1/01-fundamentos-reactjs/src/App.jsx
+++ aula1/01-fundamentos-reactjs/src/App.jsx
@@ -1,10 +1,41 @@
 import { Header } from "./components/Header"
 import { Post } from "./components/Post"
+import { Sidebar } from "./components/Sidebar";
 
 import styles from './App.module.css';
 
 import './global.css'
-import { Sidebar } from "./components/Sidebar";
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      authorUrl: 'https://github.com/Vitorlima02.png',
+      name: 'Vitor Lima',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
+      { type: 'link', content: '👉 jane.design/doctorcare', },
+    ],
+    publishedAt: new Date('2023-05-04 15:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      authorUrl: 'https://github.com/isaLima21.png',
+      name: 'Isadora Lima',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
+      { type: 'link', content: '👉 jane.design/doctorcare', },
+    ],
+    publishedAt: new Date('2023-05-03 15:00:00'),
+  }
+]
 
 export function App() {
   return (
@@ -14,7 +45,16 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
+          { posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
