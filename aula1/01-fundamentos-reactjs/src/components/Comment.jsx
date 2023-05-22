21a41d950274c78b11152980bb706e83481bb50c
Vitor Lima <vitorliman@gmail.com>
2023-05-22 11:40:34 -0300

    feat: impl deletar comentário

--- aula1/01-fundamentos-reactjs/src/components/Comment.jsx
+++ aula1/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -2,7 +2,11 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/Vitorlima02.png" />
@@ -15,7 +19,7 @@ export function Comment({ content }) {
               <time title='01 de Março às 14:30h' dateTime='2023-03-01 14:30:05'>Cerca de 1h atrás</time>
             </div>
 
-            <button title='Deletar comentário'>
+            <button onClick={handleDeleteComment} title='Deletar comentário'>
               <Trash size={24} />
             </button>
           </header>
--- aula1/01-fundamentos-reactjs/src/components/Post.jsx
+++ aula1/01-fundamentos-reactjs/src/components/Post.jsx
@@ -30,6 +30,14 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText('');
   }
 
+  function deleteComment(commentToDelete) {
+    const commentWithoutComment = comments.filter(comment => {
+      return comment !== commentToDelete
+    })
+
+    setComments(commentWithoutComment);
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -49,9 +57,9 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
         {content.map(line => {
           if (line.type === 'paragraph') {
-            return <p>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === 'link') {
-            return <p><a href="#">{line.content}</a></p>;
+            return <p key={line.content}><a href="#">{line.content}</a></p>;
           }
         })}
       </div>
@@ -71,7 +79,13 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment content={comment} />
+          return (
+              <Comment
+                key={comment}
+                content={comment}
+                onDeleteComment={deleteComment}
+              />
+            )
         })}
       </div>
     </article>
