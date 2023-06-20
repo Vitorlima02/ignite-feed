c484b2e620f581c21f8b7ad6d7ef8ef89d2b103d
Vitor Lima <vitorliman@gmail.com>
2023-06-20 20:53:32 -0300

    feat: add nova rota http

--- nodeJs/aulas/01-fundamentos-nodejs/src/server.js
+++ nodeJs/aulas/01-fundamentos-nodejs/src/server.js
@@ -1,6 +1,7 @@
 import http from 'node:http';
 import { json } from './middleware/json.js';
 import { routes } from './routes.js';
+import { extractQueryParams } from './utilities/extract-query-params.js';
 
 
 const server = http.createServer(async (req, res) => {
@@ -9,10 +10,17 @@ const server = http.createServer(async (req, res) => {
   await json(req, res)
 
   const route = routes.find(route => {
-    return route.method === method && route.path === url;
+    return route.method === method && route.path.test(url);
   });
 
   if (route) {
+    const routeParams = req.url.match(route.path)
+
+    const { query, ...params } = routeParams.groups;
+
+    req.params = params;
+    req.query = query ? extractQueryParams(query) : {};
+
     return route.handler(req, res);
   }
 
