f4e3a16b3856245386ab02d585aa4e6258fc8848
Vitor Lima <vitorliman@gmail.com>
2023-07-01 10:00:46 -0300

    feat: cria arquivo de rotas

--- nodeJs/project/src/middleware/routes.js
+++ nodeJs/project/src/middleware/routes.js
@@ -1,9 +1,43 @@
+import { randomUUID } from 'node:crypto';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR/index.js';
+import { Database } from './database.js';
+
+
+const database = new Database();
+
+const date = new Date();
+const formatDate = format(date, "d 'de' LLLL 'às' HH:mm'h'", {
+  locale: ptBR
+})
+
 export const routes = [
   {
     method: 'GET',
     path: '/tasks',
     handler: (req, res) => {
+      const tasks = database.select('tasks');
+    
+      return res.end(JSON.stringify(tasks));
+    }
+  },
+  {
+    method: 'POST',
+    path: '/tasks',
+    handler: (req, res) => {
+      const { title, description } = req.body;
+      const tasks = {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: formatDate,
+        update_at: null,
+      }
       
+      database.insert('tasks', tasks)
+
+      return res.writeHead(201).end();
     }
   }
 ]
--- nodeJs/project/src/server.js
+++ nodeJs/project/src/server.js
@@ -1,42 +1,19 @@
 import http from 'node:http';
-import { randomUUID } from 'node:crypto';
-import { format } from 'date-fns';
-import ptBR from 'date-fns/locale/pt-BR/index.js';
 import { json } from './middleware/json.js';
-import { Database } from './middleware/database.js';
+import { routes } from './middleware/routes.js';
 
-const database = new Database();
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
   await json(req, res);
 
-  if (method === 'GET' && url === '/tasks') {
-    const tasks = database.select('tasks');
-    
-    return res.end(JSON.stringify(tasks));
-  }
-
-  const date = new Date();
-  const formatDate = format(date, "d 'de' LLLL 'às' HH:mm'h'", {
-    locale: ptBR
+  const route = routes.find(route => {
+    return route.method === method && route.path === url
   })
 
-  if (method === 'POST' && url === '/tasks') {
-    const { title, description } = req.body;
-    const tasks = {
-      id: randomUUID(),
-      title,
-      description,
-      completed_at: null,
-      created_at: formatDate,
-      update_at: null,
-    }
-    
-    database.insert('tasks', tasks)
-
-    return res.writeHead(201).end();
+  if (route) {
+    return route.handler(req, res);
   }
 
   return res.writeHead(404).end();
